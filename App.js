8398d113c3a9ee970dae212bed0d78500bcd0fc4
Cyril JACQUIOD <cyriljacquiod@macbook-air-de-cyril.home>
2023-02-06 19:13:47 +0100

    Create

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3000/");
+    // console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
+  );
+}
+
+export default App;
